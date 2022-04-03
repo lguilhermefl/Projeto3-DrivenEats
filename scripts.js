817aa934b45865bb7383f854a29d9770d414dd0e
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-03 15:12:45 -0300

    adicionado bônus caixa de confirmação

--- scripts.js
+++ scripts.js
@@ -10,12 +10,14 @@ let precoSobremesa;
 
 let total;
 
+let nome;
+let endereco;
+
 /* - MARCA PRODUTO SELECIONADO POR CATEGORIA
    - ARMAZENA PRODUTO ESCOLHIDO E SEU VALOR E FAZ A SOMA DO TOTAL DO PEDIDO */
 function marcarPorCategoria(selecionado, produto, categoriaMarcada) {
-
     /* - VERIFICA SE JÁ EXISTE PRODUTO SELECIONADO NA CATEGORIA E DESMARCA CASO TENHA 
-       - REDEFINE OS VALORES ARMAZENADOS DA SELEÇÃO ANTERIOR */
+         - REDEFINE OS VALORES ARMAZENADOS DA SELEÇÃO ANTERIOR */
     if (selecionado !== null) {
         //REMOVE A CLASSE "SELECIONADO" DO PRODUTO E ESCONDE MARCADOR
         selecionado.classList.remove("selecionado");
@@ -39,17 +41,21 @@ function marcarPorCategoria(selecionado, produto, categoriaMarcada) {
     }
 
     /* - VERIFICA SE NOVA SELEÇÃO DE PRODUTO É DIFERENTE DA ANTERIOR E MARCA NOVA SELEÇÃO
-       - ARMAZENA VALORES DOS ITENS SELECIONADOS */
+         - ARMAZENA VALORES DOS ITENS SELECIONADOS */
     if (selecionado !== produto) {
-
         /* - ADICIONA A CLASSE "SELECIONADO" AO PRODUTO E MOSTRA MARCADOR
-           - TRANSFORMA MARCADOR DECIMAL DO PREÇO DE VÍRGULA PARA PONTO */
+               - TRANSFORMA MARCADOR DECIMAL DO PREÇO DE VÍRGULA PARA PONTO */
         produto.classList.add("selecionado");
         produto.querySelector(".check").classList.remove("escondido");
-        const precoProduto = parseFloat(produto.querySelector(".preco").querySelector("span").innerText.replace(',', '.'));
+        const precoProduto = parseFloat(
+            produto
+                .querySelector(".preco")
+                .querySelector("span")
+                .innerText.replace(",", ".")
+        );
 
         /* ARMAZENA OS VALORES DOS ITENS ESCOLHIDOS NA VARIÁVEIS GLOBAIS DE CADA CATEGORIA E 
-        TRANSFORMA OS PREÇOS EM NÚMEROS */
+            TRANSFORMA OS PREÇOS EM NÚMEROS */
         if (categoriaMarcada === "pratos") {
             pratoEscolhido = produto.querySelector(".titulo").innerText;
             precoPrato = Number(precoProduto);
@@ -69,13 +75,11 @@ function marcarPorCategoria(selecionado, produto, categoriaMarcada) {
     //SOMA O VALOR TOTAL DO PEDIDO, DEFINE O NÚMERO DE DECIMAIS PARA 2 DÍGITOS TRANSFORMANDO-O EM STRING NOVAMENTE
     total = precoPrato + precoBebida + precoSobremesa;
     total = total.toFixed(2);
-
 }
 
 /* - BUSCA LOCALIZAÇÃO DOS ELEMENTOS NECESSÁRIOS PARA RODAR FUNÇÃO DE MARCAR POR CATEGORIA
    - ATIVA BOTÃO DE FECHAR PEDIDO QUANDO SELECIONADAS AS 3 OPÇÕES */
 function marcarProduto(produto) {
-
     //BUSCA ELEMENTOS DE ENTRADA PARA FUNÇÃO MARCAR POR CATEGORIA
     const selecionado = produto.parentElement.querySelector(".selecionado");
     const categoriaMarcada = produto.parentElement.classList.item(1);
@@ -85,30 +89,66 @@ function marcarProduto(produto) {
     marcarPorCategoria(selecionado, produto, categoriaMarcada);
 
     //VERIFICA SE FOI ESCOLHIDA UMA OPÇÃO EM CADA CATEGORIA E ATIVA O BOTÃO EM CASO VERDADEIRO
-    if (pratoEscolhido !== undefined && bebidaEscolhida !== undefined && sobremesaEscolhida !== undefined) {
+    if (
+        pratoEscolhido !== undefined &&
+        bebidaEscolhida !== undefined &&
+        sobremesaEscolhida !== undefined
+    ) {
         botaoPedido.classList.add("botao-ativo");
         botaoPedido.querySelector("p").innerText = "Fechar Pedido";
         botaoPedido.disabled = false;
     } else {
         botaoPedido.classList.remove("botao-ativo");
-        botaoPedido.querySelector("p").innerText = "Selecione os 3 itens para fechar o pedido";
+        botaoPedido.querySelector("p").innerText =
+            "Selecione os 3 itens para fechar o pedido";
         botaoPedido.disabled = true;
     }
 }
 
 //ENVIA OS VALORES DO PEDIDO VIA MENSAGEM POR WHATSAPP
 function pedir() {
-
-    let nome = prompt("Informe quem receberá o pedido:");
-    let endereco = prompt("Informe o endereço completo de entrega:");
+    nome = prompt("Informe quem receberá o pedido:");
+    endereco = prompt("Informe o endereço completo de entrega:");
 
     //VERIFICA SE NOME E ENDEREÇO FORAM INFORMADOS
     if (nome !== "" && endereco !== "" && nome !== null && endereco !== null) {
-        let mensagemPedido = `Olá, gostaria de fazer o pedido: \n- Prato: ${pratoEscolhido}\n- Bebida: ${bebidaEscolhida}\n- Sobremesa: ${sobremesaEscolhida}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${endereco}`;
-        mensagemPedido = encodeURIComponent(mensagemPedido);
-        window.open(`https://wa.me/5532998188861?text=${mensagemPedido}`);
+        const janelaConfirmacao = document.querySelector(".confirmar-pedido");
+        const pratoConfirmar = janelaConfirmacao.querySelector(".prato-escolhido");
+        const bebidaConfirmar = janelaConfirmacao.querySelector(".bebida-escolhida");
+        const sobremesaConfirmar = janelaConfirmacao.querySelector(".sobremesa-escolhida");
+        const totalConfirmar = janelaConfirmacao.querySelector(".total-pedido");
+
+        function pontoPorVirgula (preco) {
+            return preco.toFixed(2).replace(".", ",");
+        }
+
+        let precoPratoConfirmar = pontoPorVirgula(precoPrato);
+        let precoBebidaConfirmar = pontoPorVirgula(precoBebida);
+        let precoSobremesaConfirmar = pontoPorVirgula(precoSobremesa);
+        let valorTotalConfirmar = total.replace(".", ",");
+
+        janelaConfirmacao.classList.remove("escondido");
+
+        pratoConfirmar.querySelector("p").innerText = pratoEscolhido;
+        pratoConfirmar.querySelector("span").innerText = precoPratoConfirmar;
+        bebidaConfirmar.querySelector("p").innerText = bebidaEscolhida;
+        bebidaConfirmar.querySelector("span").innerText = precoBebidaConfirmar;
+        sobremesaConfirmar.querySelector("p").innerText = sobremesaEscolhida;
+        sobremesaConfirmar.querySelector("span").innerText = precoSobremesaConfirmar;
+        totalConfirmar.querySelector("span").innerText = `R$ ${valorTotalConfirmar}`;
+        
     } else {
         alert("Por favor informe quem receberá o pedido e o endereço de entrega!");
     }
+}
 
+function confirmarPedido() {
+
+    let mensagemPedido = `Olá, gostaria de fazer o pedido: \n- Prato: ${pratoEscolhido}\n- Bebida: ${bebidaEscolhida}\n- Sobremesa: ${sobremesaEscolhida}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${endereco}`;
+    mensagemPedido = encodeURIComponent(mensagemPedido);
+    window.open(`https://wa.me/5532998188861?text=${mensagemPedido}`);
+}
+
+function cancelarPedido() {
+    document.querySelector(".confirmar-pedido").classList.add("escondido");
 }
