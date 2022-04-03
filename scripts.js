23c82732e0da345606536c04a769b6bf31fe5a77
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-03 10:59:07 -0300

    Requisitos completos, redução código js e adicionados comentários

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-
-let prato;
-let bebida;
-let sobremesa;
-
+//VARIÁVEIS GLOBAIS
 let pratoEscolhido;
 let precoPrato;
 
@@ -12,79 +8,83 @@ let precoBebida;
 let sobremesaEscolhida;
 let precoSobremesa;
 
+let total;
+
+/* - MARCA PRODUTO SELECIONADO POR CATEGORIA
+   - ARMAZENA PRODUTO ESCOLHIDO E SEU VALOR E FAZ A SOMA DO TOTAL DO PEDIDO */
 function marcarPorCategoria(selecionado, produto, categoriaMarcada) {
 
+    /* - VERIFICA SE JÁ EXISTE PRODUTO SELECIONADO NA CATEGORIA E DESMARCA CASO TENHA 
+       - REDEFINE OS VALORES ARMAZENADOS DA SELEÇÃO ANTERIOR */
     if (selecionado !== null) {
-        //REMOVE A CLASSE SELECIONADO DO PRODUTO E ESCONDE MARCADOR
+        //REMOVE A CLASSE "SELECIONADO" DO PRODUTO E ESCONDE MARCADOR
         selecionado.classList.remove("selecionado");
         selecionado.querySelector(".check").classList.add("escondido");
 
-
+        //REDEFINE VALORES ARMAZENADOS NAS VARIÁVEIS GLOBAIS DE CADA CATEGORIA
         if (categoriaMarcada === "pratos") {
-            prato = undefined;
             pratoEscolhido = undefined;
             precoPrato = undefined;
         }
 
         if (categoriaMarcada === "bebidas") {
-            bebida = undefined;
             bebidaEscolhida = undefined;
             precoBebida = undefined;
         }
 
         if (categoriaMarcada === "sobremesas") {
-            sobremesa = undefined;
             sobremesaEscolhida = undefined;
             precoSobremesa = undefined;
         }
     }
 
-    //VERIFICA SE NOVA SELEÇÃO DE PRODUTO É DIFERENTE DA ANTERIOR
+    /* - VERIFICA SE NOVA SELEÇÃO DE PRODUTO É DIFERENTE DA ANTERIOR E MARCA NOVA SELEÇÃO
+       - ARMAZENA VALORES DOS ITENS SELECIONADOS */
     if (selecionado !== produto) {
-        //ADICIONA A CLASSE SELECIONADO DO PRODUTO E MOSTRA MARCADOR
+        /* - ADICIONA A CLASSE "SELECIONADO" AO PRODUTO E MOSTRA MARCADOR
+           - TRANSFORMA MARCADOR DECIMAL DO PREÇO DE VÍRGULA PARA PONTO */
         produto.classList.add("selecionado");
         produto.querySelector(".check").classList.remove("escondido");
-        const precoProduto = parseFloat(produto.querySelector(".preco").querySelector("span").innerText.replace(',','.'));
+        const precoProduto = parseFloat(produto.querySelector(".preco").querySelector("span").innerText.replace(',', '.'));
 
+        /* ARMAZENA OS VALORES DOS ITENS ESCOLHIDOS NA VARIÁVEIS GLOBAIS DE CADA CATEGORIA E 
+        TRANSFORMA OS PREÇOS EM NÚMEROS */
         if (categoriaMarcada === "pratos") {
-            prato = "ok";
             pratoEscolhido = produto.querySelector(".titulo").innerText;
             precoPrato = Number(precoProduto);
         }
 
         if (categoriaMarcada === "bebidas") {
-            bebida = "ok";
             bebidaEscolhida = produto.querySelector(".titulo").innerText;
             precoBebida = Number(precoProduto);
         }
 
         if (categoriaMarcada === "sobremesas") {
-            sobremesa = "ok";
             sobremesaEscolhida = produto.querySelector(".titulo").innerText;
             precoSobremesa = Number(precoProduto);
         }
     }
-    console.log(pratoEscolhido);
-    console.log(precoPrato);
-    console.log(bebidaEscolhida);
-    console.log(precoBebida);
-    console.log(sobremesaEscolhida);
-    console.log(precoSobremesa);
-    let total = precoPrato + precoBebida + precoSobremesa;
+
+    //SOMA O VALOR TOTAL DO PEDIDO, DEFINE O NÚMERO DE DECIMAIS PARA 2 DÍGITOS TRANSFORMANDO-O EM STRING NOVAMENTE
+    total = precoPrato + precoBebida + precoSobremesa;
     total = total.toFixed(2);
 
-    console.log(total);
 }
 
-
+/* - BUSCA LOCALIZAÇÃO DOS ELEMENTOS NECESSÁRIOS PARA RODAR FUNÇÃO DE MARCAR POR CATEGORIA
+   - ATIVA BOTÃO DE FECHAR PEDIDO QUANDO SELECIONADAS AS 3 OPÇÕES */
 function marcarProduto(produto) {
+
+    //BUSCA ELEMENTOS DE ENTRADA PARA FUNÇÃO MARCAR POR CATEGORIA
     const selecionado = produto.parentElement.querySelector(".selecionado");
     const categoriaMarcada = produto.parentElement.classList.item(1);
+    //BUSCA ELEMENTO DO BOTÃO "FECHAR PEDIDO"
     const botaoPedido = document.querySelector(".botao-pedido");
 
     marcarPorCategoria(selecionado, produto, categoriaMarcada);
 
-    if (prato !== undefined && bebida !== undefined && sobremesa !== undefined) {
+    //VERIFICA SE FOI ESCOLHIDA UMA OPÇÃO EM CADA CATEGORIA E ATIVA O BOTÃO EM CASO VERDADEIRO
+    if (pratoEscolhido !== undefined && bebidaEscolhida !== undefined && sobremesaEscolhida !== undefined) {
         botaoPedido.classList.add("botao-ativo");
         botaoPedido.querySelector("p").innerText = "Fechar Pedido";
         botaoPedido.disabled = false;
@@ -95,6 +95,9 @@ function marcarProduto(produto) {
     }
 }
 
-
-
-
+//ENVIA OS VALORES DO PEDIDO VIA MENSAGEM POR WHATSAPP
+function pedir() {
+    let mensagemPedido = `Olá, gostaria de fazer o pedido: \n- Prato: ${pratoEscolhido}\n- Bebida: ${bebidaEscolhida}\n- Sobremesa: ${sobremesaEscolhida}\nTotal: R$ ${total}`;
+    mensagemPedido = encodeURIComponent(mensagemPedido);
+    window.open(`https://wa.me/5532998188861?text=${mensagemPedido}`);
+}
